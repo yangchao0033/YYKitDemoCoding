92b2f12bfa06569d456ae342af98602114747c18
SuperYang <757097678@qq.com>
2016-01-18 15:38:30 +0800

    创建项目

---
+++ YYKitDemoCoding/YYKitDemoCoding/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YYKitDemoCoding
+//
+//  Created by 超杨 on 16/1/18.
+//  Copyright © 2016年 杨超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
